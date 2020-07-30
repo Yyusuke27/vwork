b1ae191f9cbac9c51205fdb450439a7d91839dce
ShogoYunoki <sho.ad.nk3501@gmail.com>
2020-07-30 16:33:10 +0900

    import React にエラーが発生したのでそのための対処としてyarnをインストールした

--- frontend/src/Dashboard/Attendance/pages/KintaiManagement.tsx
+++ frontend/src/Dashboard/Attendance/pages/KintaiManagement.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CheckAttendanceArea from "../components/CheckAttendanceArea";
+import DatePickerArea from "../components/DatePickerArea";
 
 const KintaiManagement = () => {
   return (
     <>
       <CheckAttendanceArea />
+      <DatePickerArea />
     </>
   );
 };
