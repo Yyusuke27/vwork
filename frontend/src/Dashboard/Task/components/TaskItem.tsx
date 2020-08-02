d45a9b420dfe36ece9d07d9b90e9be0a05f08df6
ShogoYunoki <sho.ad.nk3501@gmail.com>
2020-08-02 12:05:36 +0900

    TaskItemをクリックした時の挙動を追加した。BackdropやDrawerなど。

--- frontend/src/Dashboard/Task/components/TaskItem.tsx
+++ frontend/src/Dashboard/Task/components/TaskItem.tsx
@@ -1,34 +1,60 @@
-import React, { FC } from "react";
-
+import React, { FC, useState } from "react";
+import Backdrop from "@material-ui/core/Backdrop";
 import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
 
 import Card from "@material-ui/core/Card";
-import { makeStyles } from "@material-ui/core/styles";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import Color from "../../../shared/util/color";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import Avatar from "@material-ui/core/Avatar";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import Drawer from "@material-ui/core/Drawer";
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-    borderRadius: 10,
-    height: 80,
-    marginBottom: 15,
-  },
+const drawerWidth = "55%";
 
-  title: {
-    fontSize: 20,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-  color: {
-    color: Color.VWORK_RED,
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      minWidth: 275,
+      borderRadius: 10,
+      height: 80,
+      marginBottom: 15,
+      cursor: "pointer",
+    },
+
+    title: {
+      fontSize: 20,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+    color: {
+      color: Color.VWORK_RED,
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerHeader: {
+      display: "flex",
+      alignItems: "center",
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: "flex-end",
+    },
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: "#fff",
+    },
+  })
+);
 
 interface TaskItemProps {
   title: string;
@@ -44,61 +70,90 @@ const TaskItem: FC<TaskItemProps> = ({
   iconType = "",
 }) => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
       <Card className={classes.root}>
-        <Grid
-          container
-          direction="row"
-          justify="space-between"
-          alignItems="center"
-          style={{ height: 80, margin: "auto" }}
-        >
-          <Grid item>
-            <Grid
-              container
-              direction="row"
-              justify="space-between"
-              alignItems="center"
-              style={{ width: 220, paddingLeft: 10 }}
-            >
-              <Grid item>
-                {iconType === "alert" ? (
-                  <ErrorOutlineIcon className={classes.color} />
-                ) : (
-                  <CheckCircleIcon />
-                )}
+        <CardActionArea onClick={handleDrawerOpen}>
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+            style={{ height: 80, margin: "auto" }}
+          >
+            <Grid item>
+              <Grid
+                container
+                direction="row"
+                justify="space-between"
+                alignItems="center"
+                style={{ width: 220, paddingLeft: 10 }}
+              >
+                <Grid item>
+                  {iconType === "alert" ? (
+                    <ErrorOutlineIcon className={classes.color} />
+                  ) : (
+                    <CheckCircleIcon />
+                  )}
+                </Grid>
+                <Grid item>
+                  <Typography className={classes.title}>{title}</Typography>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Typography className={classes.title}>{title}</Typography>
+            </Grid>
+            <Grid item>
+              <Grid
+                container
+                direction="row"
+                justify="space-between"
+                alignItems="center"
+                style={{ width: 280, paddingRight: 10 }}
+              >
+                <Grid item>
+                  <Typography>{project}</Typography>
+                </Grid>
+                <Grid item>
+                  <AccessTimeIcon />
+                </Grid>
+                <Grid item>
+                  <Typography>{data}</Typography>
+                </Grid>
+                <Grid item>
+                  <Avatar>S</Avatar>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-          <Grid item>
-            <Grid
-              container
-              direction="row"
-              justify="space-between"
-              alignItems="center"
-              style={{ width: 280, paddingRight: 10 }}
-            >
-              <Grid item>
-                <Typography>{project}</Typography>
-              </Grid>
-              <Grid item>
-                <AccessTimeIcon />
-              </Grid>
-              <Grid item>
-                <Typography>{data}</Typography>
-              </Grid>
-              <Grid item>
-                <Avatar>S</Avatar>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
+        </CardActionArea>
       </Card>
+      <Backdrop className={classes.backdrop} open={open} onClick={handleClose}>
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="right"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div
+            className={classes.drawerHeader}
+            onClick={handleDrawerClose}
+          ></div>
+        </Drawer>
+      </Backdrop>
     </>
   );
 };
