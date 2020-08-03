e9312e4f0848712ddc4abb74614deb0755536bb8
ShogoYunoki <sho.ad.nk3501@gmail.com>
2020-08-03 22:54:59 +0900

    CheckAttendanceAreaの退社Buttonをプッシュした後の画面のリファクタリング

--- frontend/src/Dashboard/Attendance/components/CheckAttendanceArea.tsx
+++ frontend/src/Dashboard/Attendance/components/CheckAttendanceArea.tsx
@@ -6,6 +6,8 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Color from "../../../shared/util/color";
 import Dialog from "@material-ui/core/Dialog";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import clsx from "clsx";
@@ -18,9 +20,7 @@ import CloseIcon from "@material-ui/icons/Close";
 import Button from "@material-ui/core/Button";
 import Drawer from "@material-ui/core/Drawer";
 import Backdrop from "@material-ui/core/Backdrop";
-import TaskInTaikinList from "../../Task/components/TaskInTaikinList";
-import TaskTodayList from "../../Task/components/TaskTodayList";
-
+import TaskList from "../../Task/components/TaskList";
 const drawerWidth = "55%";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -34,11 +34,15 @@ const useStyles = makeStyles((theme: Theme) =>
 
     title: {
       fontSize: 18,
+      // fontWeight:
     },
     maintitle: {
       m: 1,
       width: "20rem",
       fontSize: 18,
+    },
+    dialogtitle: {
+      fontSize: 30,
     },
     color: {
       backgroundColor: Color.VWORK_RED,
@@ -48,7 +52,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     appBar: {
       position: "relative",
-      backgroundColor: Color.VWORK_WHITE,
+      backgroundColor: Color.VWORK_BLACK,
     },
     dialogText: {
       fontSize: 20,
@@ -197,6 +201,18 @@ const CheckAttendanceArea = () => {
         </CardActions>
       </Card>
       <Dialog fullScreen open={open} TransitionComponent={Transition}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/logo192.png`}
+              style={{ height: 25 }}
+              alt=""
+            />
+            <Typography variant="h6" className={classes.dialogtitle}>
+              VWORK
+            </Typography>
+          </Toolbar>
+        </AppBar>
         <Grid
           container
           direction="column"
@@ -227,7 +243,11 @@ const CheckAttendanceArea = () => {
                 </Grid>
               </Grid>
             </Grid>
-            <Grid item style={{ marginTop: "3%", fontSize: 30 }}>
+            <Grid
+              item
+              style={{ marginTop: "3%", fontSize: 30, cursor: "pointer" }}
+              onClick={drawerOpen}
+            >
               X件のタスクを追加済
             </Grid>
             <Grid item>
@@ -252,9 +272,7 @@ const CheckAttendanceArea = () => {
                 justify="flex-end"
                 style={{ marginTop: "10%" }}
               >
-                <Button className={classes.takinButton} onClick={drawerOpen}>
-                  退勤する
-                </Button>
+                <Button className={classes.takinButton}>退勤する</Button>
               </Grid>
             </Grid>
           </Container>
@@ -269,21 +287,23 @@ const CheckAttendanceArea = () => {
               paper: classes.drawerPaper,
             }}
           >
-            <Grid container direction="column">
-              <Grid item>
-                <IconButton
-                  edge="start"
-                  color="inherit"
-                  onClick={handleDrawerClose}
-                  aria-label="close"
-                >
-                  <CloseIcon />
-                </IconButton>
+            <Container maxWidth="md">
+              <Grid container direction="column">
+                <Grid item>
+                  <IconButton
+                    edge="start"
+                    color="inherit"
+                    onClick={handleDrawerClose}
+                    aria-label="close"
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                </Grid>
+                <Grid item>
+                  <TaskList title="タスク" />
+                </Grid>
               </Grid>
-              <Grid item>
-                <TaskInTaikinList />
-              </Grid>
-            </Grid>
+            </Container>
           </Drawer>
         </Backdrop>
         <Backdrop className={classes.backdrop} open={drawer}>
@@ -296,21 +316,23 @@ const CheckAttendanceArea = () => {
               paper: classes.drawerPaper,
             }}
           >
-            <Grid container direction="column">
-              <Grid item>
-                <IconButton
-                  edge="start"
-                  color="inherit"
-                  onClick={drawerClose}
-                  aria-label="close"
-                >
-                  <CloseIcon />
-                </IconButton>
+            <Container maxWidth="md">
+              <Grid container direction="column">
+                <Grid item>
+                  <IconButton
+                    edge="start"
+                    color="inherit"
+                    onClick={drawerClose}
+                    aria-label="close"
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                </Grid>
+                <Grid item>
+                  <TaskList title="今日行ったタスク" />
+                </Grid>
               </Grid>
-              <Grid item>
-                <TaskTodayList />
-              </Grid>
-            </Grid>
+            </Container>
           </Drawer>
         </Backdrop>
       </Dialog>
--- frontend/src/Dashboard/Task/components/TaskList.tsx
+++ frontend/src/Dashboard/Task/components/TaskList.tsx
@@ -1,26 +1,72 @@
 import React, { FC } from "react";
 import Box from "@material-ui/core/Box";
 
-import { makeStyles } from "@material-ui/core/styles";
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+  withStyles,
+} from "@material-ui/core/styles";
 
 import TaskItem from "./TaskItem";
 import Grid from "@material-ui/core/Grid";
+import FormControl from "@material-ui/core/FormControl";
+import NativeSelect from "@material-ui/core/NativeSelect";
+import InputBase from "@material-ui/core/InputBase";
 
-const defaultProps = {
-  m: 1,
-};
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      minWidth: 275,
+      borderRadius: 10,
+      height: 80,
+    },
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-    borderRadius: 10,
-    height: 80,
-  },
+    title: {
+      fontSize: 23,
+    },
+    margin: {
+      margin: theme.spacing(1),
+    },
+  })
+);
 
-  title: {
-    fontSize: 18,
-  },
-});
+const BootstrapInput = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "label + &": {
+        marginTop: theme.spacing(3),
+      },
+    },
+    input: {
+      borderRadius: 4,
+      position: "relative",
+      backgroundColor: theme.palette.background.paper,
+      border: "1px solid #ced4da",
+      fontSize: 16,
+      padding: "10px 26px 10px 12px",
+      transition: theme.transitions.create(["border-color", "box-shadow"]),
+      // Use the system font instead of the default Roboto font.
+      fontFamily: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(","),
+      "&:focus": {
+        borderRadius: 4,
+        borderColor: "#80bdff",
+        boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
+      },
+    },
+  })
+)(InputBase);
 
 const taskData = [
   { title: "管理画面のUI見直し", project: "プロジェクト名", data: "日付" },
@@ -33,23 +79,58 @@ interface TaskListProps {
 
 const TaskList: FC<TaskListProps> = ({ title = "", iconType = "" }) => {
   const classes = useStyles();
+  const [text, setText] = React.useState("");
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setText(event.target.value as string);
+  };
 
   return (
     <>
-      {title ? (
-        <Box
-          borderBottom={1}
-          {...defaultProps}
-          className={classes.title}
-          mt={10}
-          mb={3}
-        >
-          {title}
-        </Box>
-      ) : (
-        ""
-      )}
-      <Grid container direction="column" justify="flex-start">
+      <Grid
+        container
+        direction="row"
+        justify="space-between"
+        alignItems="center"
+        style={{ marginTop: "5%" }}
+      >
+        <Grid item>
+          {title ? (
+            <Box
+              borderBottom={1}
+              className={classes.title}
+              style={{ width: 400 }}
+            >
+              {title}
+            </Box>
+          ) : (
+            ""
+          )}
+        </Grid>
+        <Grid item>
+          {title === "タスク" ? (
+            <FormControl className={classes.margin}>
+              <NativeSelect
+                value={text}
+                onChange={handleChange}
+                input={<BootstrapInput />}
+                style={{ width: 200 }}
+              >
+                <option value={"未完了のタスク"}>未完了のタスク</option>
+                <option value={"未完了のタスク"}>未完了のタスク</option>
+                <option value={"未完了のタスク"}>未完了のタスク</option>
+              </NativeSelect>
+            </FormControl>
+          ) : (
+            ""
+          )}
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        direction="column"
+        justify="flex-start"
+        style={{ marginTop: "2%" }}
+      >
         {taskData.map((data, index) => {
           return (
             <TaskItem
