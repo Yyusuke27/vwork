cd55d284fcab5a626c8567e264bf13db146d4f52
yyusuke27 <yyusuke27@gmail.com>
2020-08-03 21:50:21 +0900

    開発環境用のログ出力を追加

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// 開発環境用のログ
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 const PORT = process.env.PORT || 5000;
 
