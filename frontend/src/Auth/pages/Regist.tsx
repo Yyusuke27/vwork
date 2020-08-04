95122c22dc999d4560447ed0d2796847c383a30c
ShogoYunoki <sho.ad.nk3501@gmail.com>
2020-08-04 21:21:48 +0900

    Registのリファクタリング。withの固定を行った。

--- frontend/src/Auth/pages/Regist.tsx
+++ frontend/src/Auth/pages/Regist.tsx
@@ -5,16 +5,11 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import Slide from "@material-ui/core/Slide";
-import { TransitionProps } from "@material-ui/core/transitions";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Color from "../../shared/util/color";
-import ListItem from "@material-ui/core/ListItem";
-import { NavLink } from "react-router-dom";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import HomeIcon from "@material-ui/icons/Home";
-import ListItemText from "@material-ui/core/ListItemText";
-import List from "@material-ui/core/List";
+import { Link } from "react-router-dom";
+
+import "./Regist.css";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -28,42 +23,20 @@ const useStyles = makeStyles((theme: Theme) =>
     button: {
       backgroundColor: Color.VWORK_RED,
       color: Color.VWORK_WHITE,
-      marginTop: "30%",
-      marginLeft: "80%",
-      marginBottom: "5%",
-      width: 30,
+    },
+    style: {
+      marginTop: "40%",
+      marginRight: "10%",
     },
   })
 );
 
-const Transition = React.forwardRef(function Transition(
-  props: TransitionProps & { children?: React.ReactElement<any, any> },
-  ref: React.Ref<unknown>
-) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
-
 const Regist = () => {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
 
   return (
     <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Slide in alert dialog
-      </Button>
-      <Dialog
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        aria-labelledby="alert-dialog-slide-title"
-        aria-describedby="alert-dialog-slide-description"
-        fullWidth={true}
-      >
+      <Dialog open keepMounted fullWidth>
         <DialogTitle id="alert-dialog-slide-title">
           <h1>
             <span style={{ fontSize: 50, color: Color.VWORK_RED }}>VWORK</span>
@@ -77,9 +50,9 @@ const Regist = () => {
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <ListItem button component={NavLink} to="/auth/regist/step1" exact>
+          <Link to="/auth/regist/step1" style={{ textDecoration: "none" }}>
             <Button className={classes.button}>NEXT</Button>
-          </ListItem>
+          </Link>
         </DialogActions>
       </Dialog>
     </div>
