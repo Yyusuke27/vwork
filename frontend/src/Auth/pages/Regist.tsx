7a48e8042192f153e34b0e3ba5e8a3e4dd3056be
ShogoYunoki <sho.ad.nk3501@gmail.com>
2020-08-04 18:58:00 +0900

    Registの実装。

--- frontend/src/Auth/pages/Regist.tsx
+++ frontend/src/Auth/pages/Regist.tsx
@@ -1,7 +1,81 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
+import { TransitionProps } from "@material-ui/core/transitions";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Color from "../../shared/util/color";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    title: {
+      fontSize: 100,
+    },
+    dialog: {
+      maxHeight: "100%",
+      height: 600,
+    },
+    button: {
+      backgroundColor: Color.VWORK_RED,
+      color: Color.VWORK_WHITE,
+      marginTop: "30%",
+      marginRight: "5%",
+      marginBottom: "5%",
+      width: 30,
+    },
+  })
+);
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & { children?: React.ReactElement<any, any> },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const Regist = () => {
-  return <></>;
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Slide in alert dialog
+      </Button>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+        fullWidth={true}
+      >
+        <DialogTitle id="alert-dialog-slide-title">
+          <h1>
+            <span style={{ fontSize: 50, color: Color.VWORK_RED }}>VWORK</span>
+            へようこそ
+          </h1>
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            <p>これからVWORKを活用するための設定をして頂きます。</p>
+            <p>NEXTを押して次へ進んで下さい。</p>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button className={classes.button}>NEXT</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 };
 
 export default Regist;
