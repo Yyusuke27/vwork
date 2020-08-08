91d1694adcee66ef6074d7c98b3b90ff3f9db411
yyusuke27 <yyusuke27@gmail.com>
2020-08-08 11:24:45 +0900

    プロジェクトのmodelにタイムスタンプを追加

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -1,45 +1,50 @@
 const mongoose = require("mongoose");
 
-const ProjectSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "プロジェクト名を入力してください"],
-  },
-  description: {
-    type: String,
-    required: [true, "プロジェクト詳細を入力してください"],
-  },
-  privacy: {
-    type: Boolean,
-    default: false,
-  },
-  color: {
-    type: Number,
-    default: 0,
-  },
-  icon: {
-    type: Number,
-    default: 0,
-  },
-  workspace: {
-    type: mongoose.Schema.ObjectId,
-    ref: "Workspace",
-    required: true,
-  },
-  owners: [
-    {
+const ProjectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "プロジェクト名を入力してください"],
+    },
+    description: {
+      type: String,
+      required: [true, "プロジェクト詳細を入力してください"],
+    },
+    privacy: {
+      type: Boolean,
+      default: false,
+    },
+    color: {
+      type: Number,
+      default: 0,
+    },
+    icon: {
+      type: Number,
+      default: 0,
+    },
+    workspace: {
       type: mongoose.Schema.ObjectId,
-      ref: "User",
+      ref: "Workspace",
       required: true,
     },
-  ],
-  members: [
-    {
-      type: mongoose.Schema.ObjectId,
-      ref: "User",
-      required: true,
-    },
-  ],
-});
+    owners: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    members: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Project", ProjectSchema);
