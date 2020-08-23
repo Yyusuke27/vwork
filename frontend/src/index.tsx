6292cbfc07a06a1751b727fbe181f0a36b339b29
yyusuke27 <yyusuke27@gmail.com>
2020-08-24 00:06:31 +0900

    service workerを削除

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import { unregister } from "./serviceWorker";
 import store from "./store";
 import { Provider } from "react-redux";
 
@@ -14,7 +14,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+unregister();
