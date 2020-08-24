d143388f435e02565e4ebba269262bfea1fc80e3
yyusuke27 <yyusuke27@gmail.com>
2020-08-24 22:37:37 +0900

    メール文面を修正

--- backend/controllers/workspaces.js
+++ backend/controllers/workspaces.js
@@ -212,7 +212,7 @@ exports.inviteNewMembers = asyncHandler(async (req, res, next) => {
     const inviteeRegistUrl = `${process.env.HOST_URL}regist/invitee/welcome/?${inviteToken}`;
 
     const message = `招待からの登録はこちらから \n\n ${inviteeRegistUrl}`;
-    const html = `<a href="${inviteeRegistUrl}">${invitation.name}さん：${workspace.name}へ招待されました。登録はこちらから</a>`;
+    const html = `<p>${invitation.name}さん</p><p>${workspace.name}へ招待されました。<a href="${inviteeRegistUrl}">登録はこちらから</a></p>`;
 
     try {
       await sendEmail({
