8739d707d82bc32123a04a8b5ccc49ce498fa3be
ShogoYunoki <sho.ad.nk3501@gmail.com>
2020-08-24 19:30:02 +0900

    不要ファイルの消去

--- frontend/src/Dashboard/Task/components/TaskInProjectList.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import TaskInProjectItem from "./TaskInProjectItem";
-
-const TaskInProjectList = () => {
-  return <>{<TaskInProjectItem title={"UI画面の管理"} />}</>;
-};
-
-export default TaskInProjectList;
--- frontend/src/shared/components/Navigation/WorkSpace.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const WorkSpace = () => {
-  return <></>;
-};
-
-export default WorkSpace;
