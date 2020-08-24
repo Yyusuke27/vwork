63e9e289661fa2b102d120b0a362330478f7b4a3
yyusuke27 <yyusuke27@gmail.com>
2020-08-24 22:01:24 +0900

    タスクリストのレイアウトを修正

--- frontend/src/Dashboard/Task/components/TaskItem.tsx
+++ frontend/src/Dashboard/Task/components/TaskItem.tsx
@@ -8,7 +8,6 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import Avatar from "@material-ui/core/Avatar";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import Chip from "@material-ui/core/Chip";
@@ -85,10 +84,11 @@ const TaskItem: FC<TaskItemProps> = ({ iconType = "", data }) => {
           <Grid
             container
             alignItems="center"
+            justify="space-between"
             style={{ height: 80, padding: "10px", boxSizing: "border-box" }}
             spacing={2}
           >
-            <Grid item xs={6}>
+            <Grid item xs={5}>
               <Grid
                 container
                 wrap="nowrap"
@@ -108,39 +108,38 @@ const TaskItem: FC<TaskItemProps> = ({ iconType = "", data }) => {
                 </Grid>
               </Grid>
             </Grid>
-            <Grid item container xs alignItems="center">
-              <Grid item>
-                {data.project && data.project.name ? (
-                  <Chip
-                    label={data.project.name}
-                    variant="outlined"
-                    color="secondary"
-                  />
+            <Grid item container xs alignItems="center" justify="flex-end">
+              <Grid item container xs={8} alignItems="center" justify="center">
+                <Grid item>
+                  {data.project && data.project.name ? (
+                    <Chip
+                      label={data.project.name}
+                      variant="outlined"
+                      color="secondary"
+                    />
+                  ) : (
+                    <Chip label="プロジェクト未選択" variant="outlined" />
+                  )}
+                </Grid>
+              </Grid>
+              <Grid item container xs={3} alignItems="center" spacing={1}>
+                <Grid item>
+                  <Typography>
+                    {data.endDateAt
+                      ? moment(data.endDateAt).format("YYYY/MM/DD")
+                      : ""}
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid item xs={1}>
+                {data.user.name ? (
+                  <Avatar className={classes.avatar}>
+                    {data.user.name.slice(0, 1)}
+                  </Avatar>
                 ) : (
-                  <Chip label="プロジェクト未選択" variant="outlined" />
+                  <Avatar>V</Avatar>
                 )}
               </Grid>
-            </Grid>
-            <Grid item container xs alignItems="center" spacing={1}>
-              <Grid item>
-                <AccessTimeIcon color="disabled" />
-              </Grid>
-              <Grid item>
-                <Typography>
-                  {data.endDateAt
-                    ? moment(data.endDateAt).format("YYYY/MM/DD")
-                    : ""}
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item xs={1}>
-              {data.user.name ? (
-                <Avatar className={classes.avatar}>
-                  {data.user.name.slice(0, 1)}
-                </Avatar>
-              ) : (
-                <Avatar>V</Avatar>
-              )}
             </Grid>
           </Grid>
         </CardActionArea>
