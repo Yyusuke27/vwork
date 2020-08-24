b24416e172703f5d57ad2cd50e33b9eff3ec3778
ShogoYunoki <sho.ad.nk3501@gmail.com>
2020-08-24 16:03:00 +0900

    import読み込み箇所の修正

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { unregister } from "./serviceWorker";
 import store from "./store";
-import { Provider } from "react-redux";
 
 ReactDOM.render(
   <Provider store={store}>
