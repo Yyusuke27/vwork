1d6504bd3f9815730462c6eb606e9af61ce0e97a
yyusuke27 <yyusuke27@gmail.com>
2020-09-13 12:18:18 +0900

    annotate設定

--- backend/app/models/workspace.rb
+++ backend/app/models/workspace.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: workspaces
+#
+#  id         :bigint           not null, primary key
+#  active     :boolean
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Workspace < ApplicationRecord
 end
--- backend/spec/models/workspace_spec.rb
+++ backend/spec/models/workspace_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: workspaces
+#
+#  id         :bigint           not null, primary key
+#  active     :boolean
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Workspace, type: :model do
