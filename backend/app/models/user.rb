685a320190e112b85622a6863455097d447cf72c
yyusuke27 <yyusuke27@gmail.com>
2020-11-08 22:38:24 +0900

    リファクタリング

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -42,11 +42,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  has_many :workspace_members, foreign_key: "member_id"
+  has_many :workspace_members, foreign_key: 'member_id'
   has_many :workspaces, through: :workspace_members
-
   has_many :project_members
   has_many :projects, through: :project_members
-
   has_many :user_profiles
 end
