9a41179548777d12677e2d2f6a0286df92113685
yyusuke27 <yyusuke27@gmail.com>
2020-11-16 23:45:45 +0900

    招待者の登録処理APIの追加

---
+++ backend/app/controllers/api/v1/workspaces/invitations_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::WorkspacesController::InvitationsController < Api::ApiController
+  def index
+    # 招待者一覧
+  end
+
+  def update
+    # 招待後の登録
+  end
+
+  def auth
+    # トークンを判定
+  end
+end
---
+++ backend/app/controllers/api/v1/workspaces/members_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::WorkspacesController::MembersController < Api::ApiController
+  def index
+  end
+end
