6b88e3a06f3b5ad067b7725138862a9fb418727a
yyusuke27 <yyusuke27@gmail.com>
2020-11-19 09:09:42 +0900

    workspaceモデルのコールバックタイミングを変更

--- backend/app/models/workspace.rb
+++ backend/app/models/workspace.rb
@@ -14,7 +14,7 @@
 #  index_workspaces_on_path_id  (path_id)
 #
 class Workspace < ApplicationRecord
-  before_save :set_path_id
+  before_create :set_path_id
 
   has_many :workspace_members
   has_many :members, :class_name => 'User', :through => :workspace_members
@@ -27,6 +27,7 @@ class Workspace < ApplicationRecord
   private
 
   def set_path_id
+    # 空もしくは同じpath_idが既に存在していた場合に作成
     while self.path_id.blank? || Workspace.find_by(:path_id => self.path_id).present? do
       self.path_id = SecureRandom.alphanumeric(15)
     end
