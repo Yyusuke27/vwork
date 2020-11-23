332e75923544df2957588d04b8074d7da1854bfc
yyusuke27 <yyusuke27@gmail.com>
2020-11-23 14:31:19 +0900

    Docker環境を構築

---
+++ backend/config/initializers/redis.rb
@@ -0,0 +1,3 @@
+require 'redis'
+
+Redis.current = Redis.new(host: ENV["REDIS_HOST"], port: ENV["REDIS_PORT"])
--- backend/config/initializers/sidekiq.rb
+++ backend/config/initializers/sidekiq.rb
@@ -1,20 +1,19 @@
 require 'sidekiq'
 
-# TODO: 環境変数にSPLIT_SERVERを設定
 Sidekiq.configure_server do |config|
   case Rails.env
     when 'production' then
-      config.redis = {:url => "redis://#{ENV['SPLIT_SERVER']}:6379"}
+      config.redis = {:url => "redis://#{ENV['REDIS_HOST']}:6379"}
     else
-      config.redis = {:url => 'redis://127.0.0.1:6379'}
+      config.redis = {:url => "redis://#{ENV['REDIS_HOST']}:6379"}
   end
 end
 
 Sidekiq.configure_client do |config|
   case Rails.env
     when 'production' then
-      config.redis = {:url => "redis://#{ENV['SPLIT_SERVER']}:6379"}
+      config.redis = {:url => "redis://#{ENV['REDIS_HOST']}:6379"}
     else
-      config.redis = {:url => 'redis://127.0.0.1:6379'}
+      config.redis = {:url => "redis://#{ENV['REDIS_HOST']}:6379"}
   end
 end
