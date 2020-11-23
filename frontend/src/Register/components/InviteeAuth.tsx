6a95296229fa9dbd3dcdfd78ac06aa49e2626e10
yyusuke27 <yyusuke27@gmail.com>
2020-11-23 14:26:09 +0900

    招待者の認証画面を作成

---
+++ frontend/src/Register/components/InviteeAuth.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InviteeAuth = () => {
+  const message = '招待者の認証ページ';
+  return (
+    <div>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default InviteeAuth;
--- frontend/src/Register/pages/RegisterInvitee.tsx
+++ frontend/src/Register/pages/RegisterInvitee.tsx
@@ -1,29 +1,30 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import InviteeWelcome from "../components/InviteeWelcome";
-import InviteeStepOne from "../components/InviteeStepOne";
-import InviteeStepTwo from "../components/InviteeStepTwo";
-import InviteeStepThree from "../components/InviteeStepThree";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import InviteeWelcome from '../components/InviteeWelcome';
+import InviteeStepOne from '../components/InviteeStepOne';
+import InviteeStepTwo from '../components/InviteeStepTwo';
+import InviteeStepThree from '../components/InviteeStepThree';
 
-const RegisterInvitee = () => {
-  return (
-    <>
-      <Switch>
-        <Route path="/register/invitee/welcome">
-          <InviteeWelcome />
-        </Route>
-        <Route path="/register/invitee/step1" exact>
-          <InviteeStepOne />
-        </Route>
-        <Route path="/register/invitee/step2" exact>
-          <InviteeStepTwo />
-        </Route>
-        <Route path="/register/invitee/step3" exact>
-          <InviteeStepThree />
-        </Route>
-      </Switch>
-    </>
-  );
-};
+const RegisterInvitee = () => (
+  <>
+    <Switch>
+      <Route path="/register/invitee/auth" exact>
+        <InviteeAuth />
+      </Route>
+      <Route path="/register/invitee/welcome" exact>
+        <InviteeWelcome />
+      </Route>
+      <Route path="/register/invitee/step1" exact>
+        <InviteeStepOne />
+      </Route>
+      <Route path="/register/invitee/step2" exact>
+        <InviteeStepTwo />
+      </Route>
+      <Route path="/register/invitee/step3" exact>
+        <InviteeStepThree />
+      </Route>
+    </Switch>
+  </>
+);
 
 export default RegisterInvitee;
