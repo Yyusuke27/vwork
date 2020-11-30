e5f8c3f57206ba01b341ab7edaa75432fb83f321
yyusuke27 <yyusuke27@gmail.com>
2020-11-30 23:29:01 +0900

    projectのapiを修正

--- backend/app/controllers/api/v1/projects/my_controller.rb
+++ backend/app/controllers/api/v1/projects/my_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::Projects::MyController < Api::ApiController
   def index
-    # current_userのプロジェクト
+    workspace = Workspace.find_by(:path_id => params[:workspace_path_id])
+    projects = Project.where(:workspace_id => workspace.id, :user_id => @current_user.id)
+
+    render :template => 'api/v1/projects/my/index.json.jb', :locals => { :projects => projects }
   end
 end
