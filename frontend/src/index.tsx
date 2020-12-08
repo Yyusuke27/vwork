83580305051b5628411414dd1df278948e477e9f
yyusuke27 <yyusuke27@gmail.com>
2020-12-09 03:44:20 +0900

    Reactのエラー監視にsentryを導入

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,17 @@ import "./index.css";
 import App from "./App";
 import { unregister } from "./serviceWorker";
 import store from "./store";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <Provider store={store}>
