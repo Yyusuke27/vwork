60eed9b71ee691cd83a3d87f358d7e8d91645a5e
yyusuke27 <yyusuke27@gmail.com>
2020-12-09 03:11:37 +0900

    不要なnot_foundを削除

--- backend/app/controllers/api/v1/members/tasks_controller.rb
+++ backend/app/controllers/api/v1/members/tasks_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::Members::TasksController < Api::V1::Members::WorkspacesController
 
   def index
     tasks = Task.includes(:user, :project).where(:user_id => params[:member_id])
-    not_found if tasks.blank?
 
     render :template => 'api/v1/members/tasks/index.json.jb', :locals => {
       :tasks => tasks
